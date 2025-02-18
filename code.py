62f5d30f65aafda01021c04a14208680edd70cfe
KAUSHIK P <1nt23ad025.kaushik@nmit.ac.in>
2025-02-18 21:08:28 +0530

    Update code.py

--- code.py
+++ code.py
@@ -9,3 +9,15 @@ if answer == "left":
     answer = input("You come to a river,"
                    " you can walk around it or swim accross?"
                    "Type walk to walk around and swim to swim accross: ")
+  
+    if answer == "swim":
+        print("You swam acrross and were eaten by an alligator.")
+    elif answer == "walk":
+        print("You walked for many miles,"
+              "ran out of water and you lost the game.")
+    else:
+        print('Not a valid option. You lose.')
+
+elif answer == "right":
+    answer = input("You come to a bridge, it looks wobbly,"
+                   "do you want to cross it or head back (cross/back)? ")
